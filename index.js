5341ae0be1214b53ddadb4bb327eea6c4b5e9522
GaziMaksudur <gazimaksudur2@gmail.com>
2024-05-11 01:46:53 +0600

    jwt the dangerous and devestating issue solved

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5000",
+    "http://localhost:5173",
+    "https://trip-flip.web.app"
+  ],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
 
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? 'none' : 'strict',
+};
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.oknyghy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -25,6 +40,23 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyToken = (req, res, next)=>{
+  const token = req?.cookies?.token;
+  // console.log(req?.cookies);
+  // console.log('token at verification',token);
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'});
+  }
+  jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err,decoded)=>{
+    if(err){
+      return res.status(401).send({message: "unauthorized access!!"});
+    }
+    req.user = decoded;
+  })
+  // console.log('cookie in the middleware : ',token);
+  next();
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -35,6 +67,20 @@ async function run() {
     const hotelsDB = client.db("hotels");
     const demoColl = hotelsDB.collection("demoColl");
     const roomsColl = hotelsDB.collection("roomsColl");
+
+    app.post('/jwt', async(req, res)=>{
+        const user = req.body;
+        console.log('user for token :',user);
+        const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, { expiresIn: '1h' })
+        res.cookie('token', token, cookieOptions).send({token});
+    });
+
+    app.post('/logout', async(req, res)=>{
+      const user = req.body;
+      console.log("logged out user: ",user);
+      res.clearCookie('token',{ maxAge: 0}).send({success: true});
+    });
+
 
     app.get('/',(req, res)=>{
         res.send("Simple api Backend is running!!");
@@ -51,7 +97,13 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/rooms', async(req, res)=>{
+    app.get('/rooms', verifyToken, async(req, res)=>{
+      const user = req.user;
+      const queryUser = req.query;
+      console.log('user :',user, " and query user: ", queryUser);
+      if(user.email !== queryUser.email){
+        return res.status(403).send({message: "forbidden access!!"});
+      }
       const result = await roomsColl.find().toArray();
       res.send(result);
     });
